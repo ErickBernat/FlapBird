0b2f7067b023974267d5a6e6e3efce2d7dd96637
ErickBernat <erickduartebr@gmail.com>
2025-04-21 12:42:52 -0300

    Refactor script principal

--- Scripts/iniciaJogo.js
+++ Scripts/iniciaJogo.js
@@ -1,17 +1,25 @@
-import { iniciaJogo } from "../script.js";
+import {movimentaPassaro} from './movimentaPassaro.js';
+import { criaPredios } from './modificaPosicaoPredios.js';
+import {defineTamanhoPredios} from './loopRandomizacao.js';
+import { verificaDerrota } from './verificaDerrota.js';
+
 const botaoJogar = document.getElementById('botaoJogar');
 const telaInicial = document.getElementById('telaInicial');
 
+let tempoIniciaJogo = 3000;
 
-
-
-export function controlaInicioJogo(){
+export function iniciaJogo(){
     botaoJogar.addEventListener('click',()=>{
-        console.log('apertou')
-        iniciaJogo()
-        telaInicial.style.display = 'none'
-    })
+        telaInicial.style.display = 'none';
 
+        setInterval(() => {
+            defineTamanhoPredios();
+            criaPredios();
+            movimentaPassaro();
+            verificaDerrota();
+        }, tempoIniciaJogo);
+
+    })
 }
 
 
--- script.js
+++ script.js
@@ -1,20 +1,14 @@
-import {defineTamanhoPredios} from './Scripts/loopRandomizacao.js';
-import { verificaDerrota } from './Scripts/verificaDerrota.js';
-import {movimentaPassaro} from './Scripts/movimentaPassaro.js';
 import { reiniciaJogo } from './Scripts/reiniciaJogo.js';
-import { controlaInicioJogo } from './Scripts/iniciaJogo.js';
-import { criaPredios } from './Scripts/modificaPosicaoPredios.js';
+import { iniciaJogo } from './Scripts/iniciaJogo.js';
 import { fechaModalRegras } from './Scripts/modalRegras.js';
 
-controlaInicioJogo();
 
-export function iniciaJogo(){
-    verificaDerrota();
+
+document.addEventListener('DOMContentLoaded',()=>{
+    iniciaJogo();
     reiniciaJogo();
-    defineTamanhoPredios();
-    criaPredios();
-    movimentaPassaro();
     fechaModalRegras();
-}
+})
+
 
 
